0373f928d2d2789abbcbe5b0862f7702d4578256
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-05-29 12:34:49 +0900

    s

--- src/pages/G0-0footer.php
+++ src/pages/G0-0footer.php
@@ -1,7 +1,7 @@
 <?php require '../modules/header.php'; ?>
 
 <link rel="stylesheet" href="../css/footermenustyle.css">
-<div style="width: 100vw; height: 30vh; background-color: rgb(255, 255, 255);">
+<div style="width: 30vw; height: 10vh; background-color: rgb(255, 255, 255);">
     <button class="footermenu"><img src="../image/Destiny.png" alt=""></button>
     <button class="footermenu"><img src="../image/Chat.png" alt=""></button>
     <button class="footermenu"><img src="../image/Party.png" alt=""></button>
