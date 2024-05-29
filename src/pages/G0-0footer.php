0e617170808c0f87b77bb2882151cb2d46fbd72a
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-05-29 12:37:12 +0900

    s

--- src/pages/G0-0footer.php
+++ src/pages/G0-0footer.php
@@ -1,10 +1,10 @@
 <?php require '../modules/header.php'; ?>
 
 <link rel="stylesheet" href="../css/footermenustyle.css">
-<div style="width: 30vw; height: 10vh; background-color: rgb(255, 255, 255);">
-    <button class="footermenu"><img src="../image/Destiny.png" alt=""></button>
-    <button class="footermenu"><img src="../image/Chat.png" alt=""></button>
-    <button class="footermenu"><img src="../image/Party.png" alt=""></button>
-    <button class="footermenu"><img src="../image/Search.png" alt=""></button>
-    <button class="footermenu"><img src="../image/Profile.png" alt=""></button>
+<div style="width: 100vw; height: 10vh; background-color: rgb(255, 255, 255);">
+    <button class="footermenu"><img  class="fimg" src="../image/Destiny.png" alt=""></button>
+    <button class="footermenu"><img  class="fimg" src="../image/Chat.png" alt=""></button>
+    <button class="footermenu"><img  class="fimg" src="../image/Party.png" alt=""></button>
+    <button class="footermenu"><img  class="fimg" src="../image/Search.png" alt=""></button>
+    <button class="footermenu"><img  class="fimg" src="../image/Profile.png" alt=""></button>
 </div>
