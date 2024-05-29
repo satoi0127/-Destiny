0f743ab13a567e291ddc2836aad1f390626fef71
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-05-29 11:24:41 +0900

    a

---
+++ src/pages/G-5-2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/G-5-2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>チャット詳細画面</title>
+</head>
+<body>
+<div class="container">
+    <a href="#" class="arrow_btn arrow_01"></a>
+    <h2 class="text1">深井君</h2><br>
+    <!-- 左の吹き出し -->
+    <div class="balloon-color left">
+    <figure class="icon-color"><img src="../image/hukai.png" alt="代替えテキスト" >    
+    </figure>
+    <div class="chatting-color">
+      <p class="text-color">よろしくお願いします！</p>
+    </div>
+    </div>
+    <!-- 右の吹き出し -->
+    <div class="balloon-color right">
+    <div class="chatting-color">
+      <p class="text-color">よろしく！</p>
+      <p class="text-color">何歳ですか？</p>
+    </div>
+    </div>
+
+    <div class="balloon-color left">
+    <figure class="icon-color"><img src="../image/hukai.png" alt="代替えテキスト" >    
+    </figure>
+    <div class="chatting-color">
+      <p class="text-color">白菜^^</p>
+    </div>
+
+    <div class="balloon-color left">
+        <figure class="icon-color"><img src="../image/hukai.png" alt="代替えテキスト" >    
+        </figure>
+        <div class="chatting-color">
+          <p class="text-color">まじまじマジンガー</p>
+        </div>
+    </div>
+    
+    <footer>
+        <div class="sendbox">
+         <input type="textarea" value="" placeholder="Aa" name="sendtext" class="sendtext" style="width: 200px; height: 30px;">
+         <button><img src="../image/send.png" alt="" class="sendimg"></button>
+        </div>
+    </footer>
+</div>
+
+</body>
+</html>
