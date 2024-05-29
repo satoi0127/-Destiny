a1d5ef5a74d14bcd02638c94a23b978e21765e45
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-05-29 11:27:05 +0900

    aa

--- src/pages/G-6-2 search_result.php
+++ src/pages/G-6-2 search_result.php
@@ -3,7 +3,7 @@
 <head>
     <!-- <meta charset="UTF-8"> -->
  <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
- <link rel="stylesheet" href="../css/G-6-1 search.css">
+ <link rel="stylesheet" href="../css/G-6-2 search_result.css">
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>kaihatu</title>
 </head>
@@ -12,7 +12,7 @@
 
     <div class="form">
     <form method="get" id="form2" action="自分のサイトURL">
-        <input id="sbox3" name="s" type="text" placeholder="キーワードを入力">
+        <input id="sbox3" name="s" type="text" placeholder="狩り">
         <button id="sbtn4" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
         <!-- <link href="https://fonts.googleapis.com/earlyaccess/hannari.css" rel="stylesheet"> -->
 
@@ -20,13 +20,6 @@
     </div>
 
     
-
-    <div class="rireki">
-        <h4>検索履歴</h4>
-        <p>スポーツ</p>
-        <p>読書</p>
-        <h4>おすすめ</h4>
-    </div>
 
     <div class="box">
         <img src="../image/虎.jpg" alt="">
@@ -55,5 +48,14 @@
                 </div>
                 </div>
     
+                <div class="box">
+                    <img src="../image/虎.jpg" alt="">
+                    <div class="text">
+                             <h3>虎虎　虎虎あいNO</h3>
+                    <p>狩り</p>
+                    <p>大食い</p>
+                    </div>
+                    </div>
+        
 </body>
 </html>
