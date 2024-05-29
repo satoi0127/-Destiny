345204d037752fcc5c6806a63ec80c91859eeb10
Ryusei081 <2201125@s.asojuku.ac.jp>
2024-05-29 11:27:18 +0900

    a

---
+++ src/pages/G-3-1party.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="../css/G-3-1.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>パーティー</title>
+</head>
+<body>
+    <div class="container">
+        <img src="../image/hukai.png" alt="写真" class="photo">
+        <div class="text">
+            <input type="text" placeholder="パーティー名">
+        </div>
+        <input type="text" placeholder="詳細" class="textbox">
+        <input type="text" placeholder="メンバー" class="textboxb">
+    </div>
+    
+    
+</body>
+</html>
---
+++ src/pages/G-3-6.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="../css/G-3-6.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>パーティー</title>
+</head>
+<body>
+    <a href="#" class="arrow_btn arrow_01"></a>
+    <div class="follo"><p>フォロワー</p></div>
+    
+    <input type="checkbox"  class="checkbox-round" id="option1" name="options[]" value="option1">
+    <label for="option1"><img src="../image/hukai.png" width="50px" height="50px">名前</label><br>
+    
+
+    <input type="checkbox"  class="checkbox-round" id="option2" name="options[]" value="option2">
+    <label for="option2"><img src="../image/hukai.png" width="50px" height="50px">名前</label><br>
+    
+
+    <input type="checkbox"  class="checkbox-round" id="option3" name="options[]" value="option3">
+    <label for="option3"><img src="../image/hukai.png" width="50px" height="50px">名前</label><br>
+    
+    <div class="center">
+        <button>招待</button>
+    </div>
+</body>
+</html>
