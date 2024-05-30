72bd5b5633a41d1a3d5bc2466e24c1b54e15a6fd
kanato0407 <148302672+kanato0407@users.noreply.github.com>
2024-05-30 10:29:23 +0900

    a

--- src/pages/G-4-1.php
+++ src/pages/G-4-1.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/G-4-1.css">
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>kaihatu</title>
 </head>
 <body>
@@ -14,16 +14,16 @@
     <div class="hen">
         <img src="../image/pitu.png" alt="">
         <img src="../image/ruma.png" alt="">
-        </div>
+    </div>
     <div class="huka">
-    <img src="../image/hukai.png" alt="">
-    <h2>深井君 〇〇歳 </h2>
+        <img src="../image/hukai.png" alt="">
+        <h2>深井君 〇〇歳 </h2>
     </div>  
     <h2>自己紹介</h2>
-    <div class=ai><h4>会いたいです</h4></div>
+    <div class="ai"><h4>会いたいです</h4></div>
     <br>
     <h2>趣味</h2>
-    <div class=ai><h4>ゲーム テニス アウトドア</h4></div>
+    <div class="ai"><h4>ゲーム テニス アウトドア</h4></div>
     <?php require 'G0-0footer.php'; ?>
 </body>
 </html>
