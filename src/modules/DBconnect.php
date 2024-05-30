046fa5d7ecede58e6337082cc440c800f2b32519
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-05-30 10:59:43 +0900

    aa

--- src/modules/DBconnect.php
+++ src/modules/DBconnect.php
@@ -1,7 +1,7 @@
 <?php
-const SERVER = "mysql304.phy.lolipop.lan";
-const DBNAME = "LAA1517439-destiny";
-const USER = "LAA1517439";
-const PASS = "PAss1234";
+const SERVER = "localhost";
+const DBNAME = "destiny";
+const USER = "root";
+const PASS = "root";
 $connect = "mysql:host=" . SERVER . ";dbname=" . DBNAME . ";charset=utf8";
 ?>
