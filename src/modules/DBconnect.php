8d13247850b7770de739669bd12551661e4813ce
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-06-13 09:39:08 +0900

    Delete src/modules/DBconnect.php

--- src/modules/DBconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
- const SERVER = 'localhost';
- const DBNAME = 'destiny';
- const USER = 'root';
- const PASS = 'root';
- $connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
-//test
-?>
