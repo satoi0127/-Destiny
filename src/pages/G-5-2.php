095606a74f816739589331a0f6d7a09808df587b
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-06-13 10:39:00 +0900

    u

--- src/pages/G-5-2.php
+++ src/pages/G-5-2.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <?php require '../modules/DBconnect.php'; ?>
 <?php require "../modules/showchat.php"; ?>
 <!DOCTYPE html>
@@ -18,13 +19,23 @@
 <?php
 $pdo = new PDO($connect,USER,PASS);
   $chatroom_id=$_GET['chatid'];
-  $user_id=3;
+  $user_id=$_SESSION['user']['user_id'];
+  $sql = $pdo->prepare('select * from chatmember where user_id != ?');
+  $sql->execute([$user_id]);
+  foreach($sql as $row){
+    $otherid=$row['user_id'];
+  }
+  $sql2 = $pdo->query('select * from user where user_id = ?');
+  $sql2 ->execute([$otherid]);
+  foreach($sql as $result){
+    $other_name=$result['user_name'];
+  }
   echo $chatroom_id;
 ?>
 <div class="container" >
-  <div id="ajax">
     <a href="G-5-1.php" class="arrow_btn arrow_01"></a>
-    <h2 class="text1">深井君</h2><br>
+    <h2 class="text1"><?= $other_name?></h2><br>
+    <div id="ajax">
     <?php
 
   showchat($connect,$chatroom_id);
