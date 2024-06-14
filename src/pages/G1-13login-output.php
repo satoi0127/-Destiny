1648b14683e76ba09a1a768337bf7603ee716f46
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-06-14 10:24:01 +0900

    g

--- src/pages/G1-13login-output.php
+++ src/pages/G1-13login-output.php
@@ -1,43 +1,38 @@
-<?php session_start(); ?>
-<?phprequire "../modules/DBconnect.php";?>
-<?php require "../modules/header.php"; ?>
-
 <?php
+session_start();
+require "../modules/DBconnect.php";
+
 unset($_SESSION['user']);
-$pdo = new PDO($connect,USER,PASS);
-    $sql = $pdo->prepare('select * from user where mail_address = ?');
-    $sql->execute([$_POST["mail_address"]]);
-    foreach($sql as $row){
-        // if (password_verify($_POST['user_password'], $row['user_password'])) {
-        if($row['user_password'] == $_POST['user_password']){
-            $_SESSION['user']=[
-                'id'=>$row['user_id'], 
-                'password'=>$row['user_password'],
-                'name'=>$row['user_name'],
-                'tel'=>$row['user_tel'],
-                'address'=>$row['mail_address'], 
-                'sex'=>$row['user_sex'],
-                'coordinate_latitude'=>$row['user_coordinate_latitude'],
-                'coordinate_longitude'=>$row['user_coordinate_longitude'],
-                'current_country'=>$row['user_current_country'],
-                'current_city'=>$row['user_current_city'],
-                'current_province'=>$row['user_current_province'],
-                'current_suburb'=>$row['user_current_suburb'],
-                'age'=>$row['user_age'],
-            
-            ];
-        }
-    }
-    if(isset($_SESSION['user'])) {
-        header('Location:G-2-1destinyAll.php');
-    }else {
-        header('Location:G1-13login-input.php?hogeA=メールアドレスまたはパスワードが違います');
-        exit;
-    }
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('select * from user where mail_address = ?');
+$sql->execute([$_POST["mail_address"]]);
+foreach ($sql as $row) {
+    // if (password_verify($_POST['user_password'], $row['user_password'])) {
+    if ($row['user_password'] == $_POST['user_password']) {
+        $_SESSION['user'] = [
+            'id' => $row['user_id'],
+            'password' => $row['user_password'],
+            'name' => $row['user_name'],
+            'tel' => $row['user_tel'],
+            'address' => $row['mail_address'],
+            'sex' => $row['user_sex'],
+            'coordinate_latitude' => $row['user_coordinate_latitude'],
+            'coordinate_longitude' => $row['user_coordinate_longitude'],
+            'current_country' => $row['user_current_country'],
+            'current_city' => $row['user_current_city'],
+            'current_province' => $row['user_current_province'],
+            'current_suburb' => $row['user_current_suburb'],
+            'age' => $row['user_age'],
 
+        ];
+    }
+}
+if (isset($_SESSION['user'])) {
+    header('Location:https://aso2301334.moo.jp/-Destiny/src/pages/G-2-1destinyAll.php');
+} else {
+    header('Location:G1-13login-input.php?hogeA=メールアドレスまたはパスワードが違います');
+}
 ?>
-</body>
-</html>
 <?php
- $pdo = null;   //DB切断
- ?>
+$pdo = null; // DB切断
+?>
