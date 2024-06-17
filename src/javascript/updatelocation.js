394b25a20beaacc7e42b89998c405c3e05b18f4d
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-06-17 10:08:53 +0900

    デスティニー画面位置情報途中経過

--- src/javascript/updatelocation.js
+++ src/javascript/updatelocation.js
@@ -1,17 +1,29 @@
+let lat = 0.0;
+let lon = 0.0;
+
+function getCoordinate(pos){
+    lat = pos.coords.latitude;
+    lon = pos.coords.longitude;
+    console.log("緯度 : "+lat+" 経度 : "+lon);
+}
+
 $(function(){
     console.log("jqueryが読み込まれました。");
 
     $("#updatelocation").on("click",function(){
         console.log("位置情報更新ボタンが押されました");
-        /*
+
+        navigator.geolocation.getCurrentPosition(getCoordinate);
+
         $.ajax({
             type: 'POST',
-            url: '../modules/chat_insert.php',
+            url: '../modules/updatelocation.php',
             dataType: 'text',
-            data: {message: $("#message").val(), chatroom_id: $("#chatroom_id").val(), user_id : $("#user_id").val()},
+            data: {latitude: lat, longitude: lon, user_id : $("#user_id").val()},
         }).done(function(data){
+            $("latitude").val(lat);
+            $('longitude').val(lon);
         });
-        */
     });
 
 });
