85a12d6b844fe751891a0e865f0da1d10a549679
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-06-19 11:54:45 +0900

    位置情報途中経過

--- src/javascript/updatelocation.js
+++ src/javascript/updatelocation.js
@@ -1,29 +1,36 @@
 let lat = 0.0;
 let lon = 0.0;
+let location_data = new Array();
 
 function getCoordinate(pos){
     lat = pos.coords.latitude;
     lon = pos.coords.longitude;
     console.log("緯度 : "+lat+" 経度 : "+lon);
+    $("#latitude").html(lat);
+    $("#longitude").html(lon);
 }
 
 $(function(){
     console.log("jqueryが読み込まれました。");
 
-    $("#updatelocation").on("click",function(){
+    $("#updatelocation").click(function(){
+        
         console.log("位置情報更新ボタンが押されました");
 
         navigator.geolocation.getCurrentPosition(getCoordinate);
-
+        
+        
         $.ajax({
             type: 'POST',
             url: '../modules/updatelocation.php',
             dataType: 'text',
             data: {latitude: lat, longitude: lon, user_id : $("#user_id").val()},
         }).done(function(data){
-            $("latitude").val(lat);
-            $('longitude').val(lon);
+            location_data = data.split('|');
+            console.log("php execution successful "+data);
         });
+        
+        console.log("test");
     });
 
 });
