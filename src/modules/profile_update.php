63060df81cff4b6bd344ae9e44fede4ac73244fb
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-06-26 12:00:09 +0900

    d

--- src/modules/profile_update.php
+++ src/modules/profile_update.php
@@ -5,7 +5,9 @@
     $userid = $_SESSION['user']['id'];
     $sql = $pdo->prepare('update profile set user_starsign = ?,user_height = ?,user_blood_type =?,user_purpose =?,user_description=? where user_id = ?');
     $sql->execute([$_POST['star'],$_POST['height'],$_POST['Blood'],$_POST['purpose'],$_POST['self'],$userid]);
-    echo $_POST['interest'];
+    foreach($_POST['interest'] as $choice){
+        print "you want $choice .<br/>";
+      }
     // echo $userid;
     // echo '自己紹介：'.$_POST['self'].'<br>';
     // echo '星座：'.$_POST['star'].'<br>';
