d2bf49ad8797039469d39b6fddc9356de1cef785
Ryusei081 <2201125@s.asojuku.ac.jp>
2024-06-26 12:46:40 +0900

    チャット

--- src/pages/G-3-4.php
+++ src/pages/G-3-4.php
@@ -1,6 +1,6 @@
 <?php session_start(); ?>
 <?php require '../modules/DBconnect.php'; ?>
-
+<?php require "../modules/showchat.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/G-3-4.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <script src="../javascript/jquery-3.7.0.min.js" ></script>
+    <script src="../javascript/updatechat.js"></script>
     <title>パーティーチャット画面</title>
 </head>
 <body>
@@ -48,51 +50,22 @@ if(!isset($_POST['party_id'])){
 
 <div class="bar"><a href="./G-3-5.php" class="bars"><img src="../image/bars.png" alt="" class="barsimg"></a></div>
     <h2 class="text1"><?= $party_name?></h2><br>
-    <!-- 左の吹き出し -->
-    <div class="balloon-color left">
-    <figure class="icon-color"><img src="../image/hukai.png" alt="代替えテキスト" >    
-    </figure>
-    <div class="chatting-color">
-      <p class="text-color">よろしくお願いします！</p>
-    </div>
-    </div>
-    <!-- 右の吹き出し -->
-    <div class="balloon-color right">
-    <div class="chatting-color">
-      <p class="text-color">よろしく！</p>
-    </div>
+
+    <input type="hidden" id="chatroom_id" value=<?= $chatroom_id ?>>
+
+    <div id="ajax">
+    <?php showchat($connect,$chatroom_id); ?>
     </div>
 
-    <div class="balloon-color left">
-    <figure class="icon-color"><img src="../image/虎.jpg" alt="代替えテキスト" >    
-    </figure>
-    <div class="chatting-color">
-      <p class="text-color">よろしくー</p>
-    </div>
-
-    <div class="balloon-color left">
-        <figure class="icon-color"><img src="../image/hukai.png" alt="代替えテキスト" >    
-        </figure>
-        <div class="chatting-color">
-          <p class="text-color">まじまじマジンガーーーーー</p>
-        </div>
-    </div>
-
-    <div class="balloon-color left">
-        <figure class="icon-color"><img src="../image/虎.jpg" alt="代替えテキスト" >    
-        </figure>
-        <div class="chatting-color">
-          <p class="text-color">まじまじマジンガーーーーー</p>
-        </div>
-    </div>
-    
     <footer>
         <div class="sendbox">
-         <input type="textarea" value="" placeholder="Aa" name="sendtext" class="sendtext" style="width: 200px; height: 30px;">
-         <button><img src="../image/send.png" alt="" class="sendimg"></button>
+         <input id="message" type="textarea" value="" placeholder="Aa" name="sendtext" class="sendtext" style="width: 200px; height: 30px;">
+         <button><img src="../image/send.png" alt="" class="sendimg" id="sendbutton"></button>
         </div>
     </footer>
 </div>
+
+<script src="../javascript/sendmessage.js"></script>
 
 </body>
 </html>
