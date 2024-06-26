3fff166fc8f60cdac8bc2627428faad43752ccdd
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-06-26 12:32:59 +0900

    F

--- src/modules/profile_update.php
+++ src/modules/profile_update.php
@@ -5,6 +5,22 @@
     $userid = $_SESSION['user']['id'];
     $sql = $pdo->prepare('update profile set user_starsign = ?,user_height = ?,user_blood_type =?,user_purpose =?,user_description=? where user_id = ?');
     $sql->execute([$_POST['star'],$_POST['height'],$_POST['Blood'],$_POST['purpose'],$_POST['self'],$userid]);
+
+    foreach($_POST['interest'] as $choice){
+    $userint = $pdo->prepare('INSERT INTO userInterest(uinterest_id, user_id, interest_id) VALUES (null,?,?)');
+    $userint -> execute($userid,$choice);
+    }
+    $userint2 = $pdo->prepare('select * from userInterest where user_id = ?');
+    $userint2 -> execute([$userid]);
+        foreach($userint2 as $value){
+            foreach($_POST['interest'] as $choice){
+            if($choice != $value['interest_id']){
+                
+            }
+            $intid[] =$value['interest_id']; 
+            }
+
+        }
     // foreach($_POST['interest'] as $choice){
     //     print "you want $choice .<br/>";
     //   }
