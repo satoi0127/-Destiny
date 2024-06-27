e3d7c0e091c656f095b668556d2f00a47c65afec
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-06-27 16:15:15 +0900

    位置情報修正

--- src/pages/G-2-2destinyNearUser.php
+++ src/pages/G-2-2destinyNearUser.php
@@ -3,7 +3,7 @@
 <?php require "../modules/calc_dist.php"; ?>
 <?php require "../modules/header.php"; ?>
 
-    <link rel="stylesheet" href="../css/G-2-2.css">
+    <link rel="stylesheet" href="../css/G-2destiny.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script type="text/javascript" src="../javascript/jquery-3.7.0.min.js" ></script>
 
@@ -46,7 +46,7 @@
         $suburb = $location['user_current_suburb'];
         ?>
 
-        <div id="user_location">
+        <div id="user_location" style="text-align:center;">
         <p class="aaa">現在地 <button id="updatelocation"><i class="fa fa-refresh" aria-hidden="true"></i></button> </p>
         <p class="aaa">地名:<div class="aaa" id="place_name"><?= $country ?> <?= $city ?> <?= $province ?> <?= $suburb ?></div></p>
         </div>
@@ -78,15 +78,19 @@
         <form name="individual_user<?=$individual_num?>" id="individual_user<?=$individual_num?>" action="G-4-1.php" method="GET">
             <input type="hidden" name="user_id" value="<?=$user_data['user_id']?>">
                 <div class="user_list_individual" onClick="document.forms['individual_user<?=$individual_num?>'].submit();">
+                    <?php if($dist!=-1)echo '<p style="margin:0; padding:0; text-align:right;">',$dist,' km</p>'; ?>
                     <div class="image_and_name">
                         <img src="../image/<?=$profile['user_profile_image_path']?>" alt="" class="user_list_individual_image" style="background-color: gainsboro; width: 64px; height: 64px; border-radius: 15%;">
-                        <p style="font-size: 18px;"><?=$user_data['user_name']?></p>
-                    </div>
-                    <p style="font-size: 12px;">
+                        <p style="font-size: 18px;"><?=$user_data['user_name']?></p><br>
+                        <br>
+                        <p style="font-size: 12px;">
+                            <br>
+                            <br>
                         <?=$profile['user_description']?>
                         <br>
-                        <?php echo '',$user_data['user_current_country'],' ',$user_data['user_current_city'],''?>
-                    </p>
+                        <?php echo '',$user_data['user_current_country'],' ',$user_data['user_current_city'],' ',$user_data['user_current_province'],' ',$user_data['user_current_suburb']?>
+                        </p>
+                    </div>
                 </div>
         </form>
 
