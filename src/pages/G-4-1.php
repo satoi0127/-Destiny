26ae0ea948fb80bcc84f455363ebcaa84f3f0543
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-06-28 10:12:58 +0900

    aa

--- src/pages/G-4-1.php
+++ src/pages/G-4-1.php
@@ -112,13 +112,11 @@ $purposes = array(0 =>'暇つぶし',1 =>'恋人探し',2 =>'友達探し', 3 =>
     </div>
 
 <?php if ($isCurrentUser): ?>
-    <div class="hen">
+    
     <a href="G-4-2.php">
-    <img src="../image/pitu.png" alt=""></a>
-    </div>
-    <div class="rog">
+    <img  class="hen" src="../image/pitu.png" alt=""></a>
+    
     <a href="logout.php"  class="btn btn--orange btn--radius">ログアウト</a>
-    </div>  
     <?php else: ?>
         <p class="chat"><a href="G-5-2.php?user_id=<?php echo $profileUserId; ?>"><img src="../image/image.png" alt=""> </a></p>
     <?php endif; ?>
