1a8279ec4de494dac88652b93243210fdd0e9caf
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-07-01 10:23:00 +0900

    j

--- src/pages/profile_image.php
+++ src/pages/profile_image.php
@@ -6,6 +6,8 @@ $pdo = new PDO($connect,USER,PASS);
 
 $sql = $pdo->prepare('select * from profile where user_id = ?');
 $sql->execute([$user_id]);
-foreach()
+foreach($sql as $userdate){
 $profile_image_path = $userdata['user_profile_image_path'];
+}
+?>
 <img  class="profile" src="../image/<?php echo $profile_image_path; ?>"  alt="プロフィール画像">
