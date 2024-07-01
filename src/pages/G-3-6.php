de87b07ecbb3293fdea440b6ee2c9347746b67b7
Ryusei081 <2201125@s.asojuku.ac.jp>
2024-07-01 10:59:32 +0900

    g-3-6

--- src/pages/G-3-6.php
+++ src/pages/G-3-6.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+<?php require '../modules/DBconnect.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,7 +12,37 @@
 <body>
     <a href="./G-3-5.php" class="arrow_btn arrow_01"></a>
     <div class="follo"><p>フォロワー</p></div>
+    <?php
+        $pdo = new PDO($connect,USER,PASS);
+
+        $sql = $pdo->prepare("select * from party");
     
+        $sql ->execute([]);
+        
+        foreach($sql as $results){
+
+            $sql2 = $pdo->prepare('select interest_id from partyInterest where party_id = ?');
+            $sql2 -> execute([$results['party_id']]);
+            foreach($sql2 as $row){
+                $sql3 = $pdo->prepare('select * from interest where interest_id = ?');
+                $sql3 -> execute([$row['interest_id']]);
+                foreach($sql3 as $row2){
+                    echo '<label class="syumi">'.$row2['interest_name'].'</label>';
+                }
+            }
+        $members = $pdo->prepare("SELECT user_id FROM chatmember WHERE chatmember_id = ?");
+            $members->execute([$results['chat_member_id']]);
+
+            foreach($members as $users){
+                $query = $pdo->prepare("SELECT user_profile_image_path FROM profile WHERE user_id = ?");
+                $query->execute([$users['user_id']]);
+                $imagepath = $query->fetchAll()[0]['user_profile_image_path'];
+                echo '<input type="checkbox"  class="checkbox-round" id="option1" name="options[]" value="option1">';
+                echo '<img class="user_img" src="../image/',$imagepath,'" alt="">';
+            }
+        }    
+    ?>
+    <!--
     <input type="checkbox"  class="checkbox-round" id="option1" name="options[]" value="option1">
     <label for="option1"><img src="../image/hukai.png" width="50px" height="50px">名前</label><br>
     
@@ -21,7 +53,7 @@
 
     <input type="checkbox"  class="checkbox-round" id="option3" name="options[]" value="option3">
     <label for="option3"><img src="../image/hukai.png" width="50px" height="50px">名前</label><br>
-    
+    -->
     <div class="center">
         <button onclick="location.href='./G-3-4.php'">招待</button>
     </div>
