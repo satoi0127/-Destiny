d3772bb3cb26199e09966028be866bf6f0dc406c
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-07-04 09:50:28 +0900

    aa

--- src/pages/G-4-1.php
+++ src/pages/G-4-1.php
@@ -1,10 +1,18 @@
 <?php session_start(); ?>
 <?php require "../modules/DBconnect.php"; ?>
 <?php $pdo = new PDO($connect,USER,PASS); ?>
-<?php require "../modules/header.php"; ?>
-
-<link rel="stylesheet" href="../css/G-4-1.css?v=<?php echo time(); ?>" >
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/G-4-1.css?v=<?php echo time(); ?>" >
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <title>ログイン</title>
+</head>
+<body>
+
 
 <body>
 <!-- → -->
