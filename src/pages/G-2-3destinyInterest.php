f41f1f87b2de72564faa50833ffa8830e642ad6d
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-07-04 10:27:27 +0900

    s

--- src/pages/G-2-3destinyInterest.php
+++ src/pages/G-2-3destinyInterest.php
@@ -6,6 +6,8 @@
 
     <link rel="stylesheet" href="../css/G-2destiny.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="../javascript/jquery-3.7.0.min.js" ></script>
 
     <a href="#" class="arrow_btn arrow_01"></a>
