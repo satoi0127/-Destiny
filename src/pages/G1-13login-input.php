00d94bec9364aebddb21d12892dc90b1959cd724
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-07-05 09:36:53 +0900

    aa

--- src/pages/G1-13login-input.php
+++ src/pages/G1-13login-input.php
@@ -1,6 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
-<link rel="stylesheet" href="../css/login.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>ログイン</title>
+</head>
+<body>
+
 
 <div class="login">
   <div class="login-triangle"></div>
